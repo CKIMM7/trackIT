29331c9ab2503a55da4a5cbceeaba88250b4bfd4
Anna <annah704@gmail.com>
2022-11-11 19:05:35 +0000

    added dummy data

---
+++ db/1_setup.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS user_habits;
+DROP TABLE IF EXISTS habit;
+
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    email varchar(50) NOT NULL,
+    password varchar(30) NOT null
+);
+
+CREATE TABLE user_habits (
+    id serial PRIMARY KEY,
+    user_id int,
+    habit_id int
+);
+
+CREATE TABLE habit (
+    id serial PRIMARY KEY,
+    name varchar(30) NOT NULL,
+    description varchar(300),
+    frequency INT,
+    start_date varchar(20),
+    last_completed varchar(20),
+    streak INT,
+    completed boolean
+);
