ba0f5b94437f8fd0e168fbe87586388f85e9321e
YKIM <dyounggkim@gmail.com>
2022-11-14 12:45:52 +0000

    pool connect added

--- api/dbConfig.js
+++ api/dbConfig.js
@@ -1,5 +1,29 @@
-const { Pool } = require("pg");
+const { Pool, Client } = require("pg");
+const fs = require('fs');
 
-const pool = new Pool();
+const pool = new Pool({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'root',
+    database: 'demo'
+  })
+
+const client = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'root',
+    database: 'demo'
+  })
+
+
+client.connect(function(err) {
+    if (err) throw err;
+    console.log("Client Connected!");
+  });  
+
+const seeds = fs.readFileSync(__dirname + '/2_seed.sql').toString();
+pool.query(seeds, () => console.log('Dev database seeded'));
 
 module.exports = pool;
