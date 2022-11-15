bf55338a885fb897075324a5afe789598adf079a
Anna <annah704@gmail.com>
2022-11-15 12:01:12 +0000

    fixed file path & updating profile files

--- client/assets/js/profile-script.js
+++ client/assets/js/profile-script.js
@@ -1,25 +1,32 @@
+const main = document.querySelector('main')
 const editBtn = document.querySelector('#edit-btn');
 const passBtn = document.querySelector('#pass-btn');
+
+const editProfSection = document.querySelector('#edit-profile');
+const changePassSection = document.querySelector('#change-pass');
 
 editBtn.addEventListener('click', editProfile);
 passBtn.addEventListener('click', changePass)
 
-function displayName(){
+async function displayName(){
+    const name = document.querySelector('#profile-name');
 
 }
 
-function displayEmail(){
+async function displayEmail(){
 
 }
 
 function editProfile(e){
-
+    e.preventDefault()
+    editProfSection.style.display = 'block';
 }
 
 function changePass(e){
-    
+    e.preventDefault()
+    changePassSection.style.display = 'block';
 }
 
-function displayHabits(){
+async function displayHabits(){
 
 }
